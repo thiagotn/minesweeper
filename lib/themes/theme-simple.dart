9acd647b86e41b536e9839bf777a6fdfa3c36532
Thiago Nogueira <thiagotn@gmail.com>
2021-04-17 15:32:27 -0300

    feat: alternative theme

---
+++ lib/themes/theme-simple.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+const brightness = Brightness.light;
+const primaryColor = Colors.grey;
+const accentColor = const Color(0xFFFFFFFF);
+const primarySwatch = Colors.grey;
+
+ThemeData appThemeSimple() {
+  return ThemeData(
+    brightness: brightness,
+    primaryColor: primaryColor,
+    accentColor: accentColor,
+    primarySwatch: primarySwatch,
+  );
+}
+
+BoxDecoration buildBoxDecorationScoreBoard() {
+  return BoxDecoration(
+    color: Colors.red.shade900,
+    border: Border(
+      top: BorderSide(color: Colors.red.shade50, width: 3.0),
+      left: BorderSide(color: Colors.red.shade50, width: 3.0),
+      bottom: BorderSide(color: Colors.red.shade300, width: 3.0),
+      right: BorderSide(color: Colors.red.shade300, width: 3.0),
+    ),
+  );
+}
+
+BoxDecoration buildBoxDecorationClicked() {
+  return BoxDecoration(
+    color: Color(0xffbcbcbc),
+  );
+}
+
+BoxDecoration buildBoxDecorationIn() {
+  return BoxDecoration(
+    color: Color(0xffbcbcbc),
+    border: Border(
+      top: BorderSide(color: Colors.grey, width: 3.0),
+      left: BorderSide(color: Colors.grey, width: 3.0),
+      bottom: BorderSide(color: Colors.white, width: 3.0),
+      right: BorderSide(color: Colors.white, width: 3.0),
+    ),
+  );
+}
+
+BoxDecoration buildBoxDecorationOut() {
+  return BoxDecoration(
+    color: Color(0xeeeeee),
+    border: Border(
+      top: BorderSide(color: Colors.white, width: 3.0),
+      left: BorderSide(color: Colors.white, width: 3.0),
+      bottom: BorderSide(color: Colors.grey, width: 3.0),
+      right: BorderSide(color: Colors.grey, width: 3.0),
+    ),
+  );
+}
