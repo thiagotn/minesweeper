17449c2b5a8b6cc544a1a380637f7899fef8daf6
Thiago Nogueira <thiagotn@gmail.com>
2021-04-17 15:32:52 -0300

    feat: alternative theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:minesweeper/blocs/game.bloc.dart';
 import 'package:minesweeper/pages/game.page.dart';
+import 'package:minesweeper/themes/theme-simple.dart';
 import 'package:minesweeper/themes/theme.dart';
 import 'package:provider/provider.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Campo Minado',
-        theme: appTheme(),
+        theme: appThemeSimple(),
         home: GamePage(),
         debugShowCheckedModeBanner: false,
       ),
