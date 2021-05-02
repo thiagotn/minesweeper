050d40d5cde9438d80cab3b74ed2d8c9636b1e30
Thiago Nogueira <thiagotn@gmail.com>
2021-05-02 18:38:15 -0300

    fix: scroll game.page

--- lib/widgets/game/game.widget.dart
+++ lib/widgets/game/game.widget.dart
@@ -14,6 +14,7 @@ class Game extends StatelessWidget {
         alignment: Alignment.center,
         decoration: buildBoxDecorationIn(),
         child: GridView.builder(
+          physics: ScrollPhysics(),
           shrinkWrap: true,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: bloc.rows,
